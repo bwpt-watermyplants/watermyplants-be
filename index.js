5729d993433269bd9c0fe7e767f49584bcb7c3dc
aurelions <trevorm157@hotmail.com>
2021-09-17 23:55:40 -0500

    tedious work

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require("./server")
 
 const port = process.env.PORT || 3000
 
+server.get("/", (req, res) => {
+	res.send("yoyo");
+});
+
 server.listen(port, () => {
 	console.log(`Running at http://localhost:${port}`)
 })
