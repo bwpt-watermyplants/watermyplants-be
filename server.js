3553cd332f929d470051a393ba1cfd587629a063
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-09-21 21:51:30 -0400

    BUGFIX: Installed missing packages, fixed typos in imports

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const session = require("express-session")
-const KnexSessionStore = require("connect-session-knex")(session)
-const usersRouter = require("./routers/users.js")
-const plantsRouter = require("./routers/plants_router.js")
-const speciesRouter = require("./routers/species_router.js")
-const db = require("./data/config")
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const usersRouter = require('./routers/users.js');
+const plantsRouter = require('./routers/plants-router.js');
+const speciesRouter = require('./routers/species-router.js');
+const db = require('./data/config');
 
-const server = express()
+const server = express();
 
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
-server.use(session({
-	resave: false, // avoid recreating sessions that have not changed
-	saveUninitialized: false, // only create sessions for users that are logged in
-	secret: "keep it secret keep it safe", // cryptographically sign the session cookie
-	store: new KnexSessionStore({
-		knex: db, // configured instance of knex
-		createtable: true, // create a session table automatically, rather than doing a migration for it
-	}),
-}))
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(
+  session({
+    resave: false, // avoid recreating sessions that have not changed
+    saveUninitialized: false, // only create sessions for users that are logged in
+    secret: 'keep it secret keep it safe', // cryptographically sign the session cookie
+    store: new KnexSessionStore({
+      knex: db, // configured instance of knex
+      createtable: true, // create a session table automatically, rather than doing a migration for it
+    }),
+  })
+);
 
-server.use(usersRouter)
-server.use(plantsRouter)
-server.use(speciesRouter)
+server.use(usersRouter);
+server.use(plantsRouter);
+server.use(speciesRouter);
 
 server.use((err, req, res, next) => {
-	console.log(err)
+  console.log(err);
 
-	res.status(500).json({
-		message: "Something went wrong",
-	})
-})
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
 
-module.exports = server
+module.exports = server;
