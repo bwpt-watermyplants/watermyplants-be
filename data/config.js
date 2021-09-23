0d8650dd9fc3840d4a2dd92c00292d9fdce21cd9
aurelions <trevorm157@hotmail.com>
2021-09-22 21:12:22 -0500

    done?

--- data/config.js
+++ data/config.js
@@ -1,4 +1,7 @@
+const { config } = require("dotenv")
 const knex = require("knex")
 const knexfile = require("../knexfile")
+
+const db = knex(process.env.NODE_ENV === 'production' ? config.production : config.development)
 
 module.exports = knex(knexfile.development)
--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,33 @@
+require('dotenv').config();
+
+const pg = require('pg');
+
+if(process.env.DATABASE_URL) {
+	pg.defaults.ssl = { rejectUnauthorized: false}
+};
+
+const sharedConfig = {
+	client: 'pg',
+	migrations: { directory: './data/migratioins'},
+	seeds: { directory: './data/seeds'},
+}
+
 module.exports = {
 	development: {
-		client: "sqlite3",
-		useNullAsDefault: true, // needed for sqlite
-		connection: {
-			filename: "./data/users.db3",
-		},
-		migrations: {
-			directory: "./data/migrations",
-		},
-		seeds: {
-			directory: "./data/seeds",
-		},
-
-		// this is needed when using foreign keys
-		pool: {
-			afterCreate: (conn, done) => {
-				// runs after a connection is made to the sqlite engine
-				conn.run("PRAGMA foreign_keys = ON", done) // turn on FK enforcement
-			},
-		},
+		...sharedConfig,
+		connection: process.env.DATABASE_URL,
 	},
+	production: {
+		...sharedConfig,
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+				rejectUnauthorized: false,
+			}
+		},
+		pool: {
+			min: 2,
+			max: 10
+		}
+	}
 }
